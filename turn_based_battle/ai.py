2d0b87ecded4b8bd4402cb2f70246e517de130c5
Coosome Joe <thecoltonjelsema@gmail.com>
2016-09-30 10:06:25 -0700

    Quick AI fixes
    Made ai.py work with defs.py

--- turn_based_battle/ai.py
+++ turn_based_battle/ai.py
@@ -1,24 +1,31 @@
 import random
+import defs
 #thisbattler = runAI(thisbattler, battlers1, battlers2)
 
 #Make note that hitall effect needs to be applied in your ai.
 #take in thisbattler, battlers1, battlers2
 def runAI(player, battlersL, battlersR):
 
-	consort, hpsort = [], []
+	consort, dmgtaken, hptotal = [], [], []
 	for i in battlersL:
 		consort.append(i)
-		hpsort.append(i)
+		dmgtaken.append(i)
+		hptotal.append(i)
 	#
 	for i in range(len(consort)):
 		for j in range(len(consort)-1-i):
 			if consort[j].con < consort[j+1].con:
 				consort[j], consort[j+1] = consort[j+1], consort[j]
 	#greatest to least damage taken
-	for i in range(len(hpsort)):
-		for j in range(len(hpsort)-1-i):
-			if hpsort[j].maxhp-hpsort[j].hp < hpsort[j+1].maxhp-hpsort[j+1].hp:
-				hpsort[j], hpsort[j+1] = hpsort[j+1], hpsort[j]
+	for i in range(len(dmgtaken)):
+		for j in range(len(dmgtaken)-1-i):
+			if dmgtaken[j].maxhp-dmgtaken[j].hp < dmgtaken[j+1].maxhp-dmgtaken[j+1].hp:
+				dmgtaken[j], dmgtaken[j+1] = dmgtaken[j+1], dmgtaken[j]
+	#greatest to least hp
+	for i in range(len(hptotal)):
+		for j in range(len(hptotal)-1-i):
+			if hptotal[j].hp < hptotal[j+1].hp:
+				hptotal[j], hptotal[j+1] = hptotal[j+1], hptotal[j]
 	#Suggested order:
 	'''
 	test quantity of allies and other related stuff
@@ -30,12 +37,12 @@ def runAI(player, battlersL, battlersR):
 	if player.name == "Catsome":
 		allies, prefferedtarget = len(battlersR), "self"
 
+		#solo
 		if allies == 1 and player.savingfor == "none":
 			#selecting skill
 			if player.maxhp-player.hp > 400 and random.randint(0, 2) == 1 and player.savingfor == "none":
 				player.savingfor = "heal"
-			negeffects = [burn, magicmute, bleed, poison, confusion]
-			for i in negeffects:
+			for i in defs.negeff:
 				if i in player.effects:
 					player.savingfor = "rebuke"
 			if player.savingfor == "none" and random.randint(0, 1) == 1:
@@ -43,9 +50,17 @@ def runAI(player, battlersL, battlersR):
 			if rebuff in player.effects or player.savingfor == "none":
 				player.savingfor = "attack"
 				prefferedtarget = "enemy"
-				
+		
+		#Multiple allies
 		if allies >= 2:
-			pass
+			if player.savingfor == "none" or player.savingfor == "heal":
+				for i in hptotal:
+					if i.hp < 800:
+						player.savingfor = "heal"
+						player.target = i
+				if player.hp <= 200:
+					player.prefferedtarget = "self"
+
 
 		#applying savingfor
 		if player.savingfor == "rebuke" and player.power >= 1:
@@ -65,7 +80,7 @@ def runAI(player, battlersL, battlersR):
 			player.target = [player]
 		if prefferedtarget == "enemy":
 			if player.goskill == player.skills[1]:
-				player.target = [hpsort[len(hpsort)-1]]
+				player.target = [dmgtaken[len(dmgtaken)-1]]
 			else:
 				player.target = [consort[0]]
 
@@ -83,12 +98,12 @@ def runAI(player, battlersL, battlersR):
 			if player.savingfor == "consume" or player.crit >= 30:
 				player.goskill, player.savingfor, player.target = player.skills[6], "none", [consort[0]]
 			else:
-				player.goskill, player.savingfor, player.target = player.skills[5], "none", [hpsort[len(hpsort)-1]]
+				player.goskill, player.savingfor, player.target = player.skills[5], "none", [dmgtaken[len(dmgtaken)-1]]
 		#if not enough power to use
 		else:
 			player.goskill = player.skills[rand]
 			if player.crit >= 8:
-				player.goskill, player.target = player.skills[1], [hpsort[len(hpsort)-1]]
+				player.goskill, player.target = player.skills[1], [dmgtaken[len(dmgtaken)-1]]
 			else:
 				player.target = [consort[0]]
 
